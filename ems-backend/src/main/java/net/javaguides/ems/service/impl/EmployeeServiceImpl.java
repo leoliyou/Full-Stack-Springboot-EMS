f37dfcb44039106b8692ef37e95a93e41fb6b022
leoliyou <liyouleoleo@gmail.com>
2023-09-27 23:33:22 +0100

    method

--- ems-backend/src/main/java/net/javaguides/ems/service/impl/EmployeeServiceImpl.java
+++ ems-backend/src/main/java/net/javaguides/ems/service/impl/EmployeeServiceImpl.java
@@ -16,6 +16,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public EmployeeDto createEmployee(EmployeeDto employeeDto) {
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
-        return null;
+        Employee savedEmployee employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(savedEmployee);
     }
 }
