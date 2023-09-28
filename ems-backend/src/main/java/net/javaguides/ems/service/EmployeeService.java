468d7ea4080cfb7ef5557569182e8fff18831560
leoliyou <liyouleoleo@gmail.com>
2023-09-28 23:54:40 +0100

    getId

--- ems-backend/src/main/java/net/javaguides/ems/service/EmployeeService.java
+++ ems-backend/src/main/java/net/javaguides/ems/service/EmployeeService.java
@@ -1,8 +1,11 @@
 package net.javaguides.ems.service;
 
 import net.javaguides.ems.dto.EmployeeDto;
+import net.javaguides.ems.entity.Employee;
 
 public interface EmployeeService {
 
     EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+    EmployeeDto getEmployeeById(Long employeeId);
 }
