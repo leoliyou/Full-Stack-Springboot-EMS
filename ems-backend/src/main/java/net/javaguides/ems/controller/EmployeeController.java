8b0b27e7f05c6b86493d3b46ef72dfee7c156a05
leoliyou <liyouleoleo@gmail.com>
2023-09-29 00:37:03 +0100

    deleted

--- ems-backend/src/main/java/net/javaguides/ems/controller/EmployeeController.java
+++ ems-backend/src/main/java/net/javaguides/ems/controller/EmployeeController.java
@@ -44,4 +44,12 @@ public class EmployeeController {
         return ResponseEntity.ok(employeeDto);
     }
 
+
+    // Build Delete Employee REST API
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long employeeId){
+        employeeService.deleteEmployee(employeeId);
+        return ResponseEntity.ok("Employee deleted successfully!.");
+    }
+
 }
--- ems-backend/src/main/java/net/javaguides/ems/service/EmployeeService.java
+++ ems-backend/src/main/java/net/javaguides/ems/service/EmployeeService.java
@@ -15,4 +15,6 @@ public interface EmployeeService {
 
     EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee);
 
+    void deleteEmployee(Long employeeId);
+
 }
