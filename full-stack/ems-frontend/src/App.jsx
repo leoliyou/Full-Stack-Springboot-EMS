c8e80773be7fdd987f0fd1899b4f55a284ca2762
leoliyou <liyouleoleo@gmail.com>
2023-09-29 14:49:05 +0100

    bootstrap

--- full-stack/ems-frontend/src/App.jsx
+++ full-stack/ems-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ListEmployeeComponent from "./components/ListEmployeeComponent";
 
 function App() {
   const [count, setCount] = useState(0)
