b79b4f7890ff072e01a0539bceab8bd16c40723a
leoliyou <liyouleoleo@gmail.com>
2023-09-30 23:56:03 +0100

    frontenddepartment

--- full-stack/ems-frontend/src/App.jsx
+++ full-stack/ems-frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import "./App.css";
+import DepartmentComponent from "./components/DepartmentComponent";
 import EmployeeComponent from "./components/EmployeeComponent";
 import FooterComponent from "./components/FooterComponent";
 import HeaderComponent from "./components/HeaderComponent";
+import ListDepartmentComponent from "./components/ListDepartmentComponent";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -28,6 +30,20 @@ function App() {
           <Route
             path="/edit-employee/:id"
             element={<EmployeeComponent />}></Route>
+
+          {/* // http://localhost:3000/departments */}
+          <Route
+            path="/departments"
+            element={<ListDepartmentComponent />}></Route>
+
+          {/* // http://localhost:3000/add-department */}
+          <Route
+            path="/add-department"
+            element={<DepartmentComponent />}></Route>
+
+          <Route
+            path="/edit-department/:id"
+            element={<DepartmentComponent />}></Route>
         </Routes>
         <FooterComponent />
       </BrowserRouter>
