f1756c66ce7711f0ec787966fef8db25b537971a
leoliyou <liyouleoleo@gmail.com>
2023-10-03 20:03:48 +0100

    frontendlogin

--- todo-ui/src/App.jsx
+++ todo-ui/src/App.jsx
@@ -3,10 +3,23 @@ import "./App.css";
 import ListTodoComponent from "./components/ListTodoComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import TodoComponent from "./components/TodoComponent";
+import RegisterComponent from "./components/RegisterComponent";
+import LoginComponent from "./components/LoginComponent";
+import { isUserLoggedIn } from "./services/AuthService";
 
 function App() {
+  function AuthenticatedRoute({ children }) {
+    const isAuth = isUserLoggedIn();
+
+    if (isAuth) {
+      return children;
+    }
+
+    return <Navigate to="/" />;
+  }
+
   return (
     <>
       <BrowserRouter>
@@ -15,19 +28,40 @@ function App() {
           {/* http://localhost:8080 */}
           <Route
             path="/"
-            element={<ListTodoComponent />}></Route>
+            element={<LoginComponent />}></Route>
           {/* http://localhost:8080/todos */}
           <Route
             path="/todos"
-            element={<ListTodoComponent />}></Route>
+            element={
+              <AuthenticatedRoute>
+                <ListTodoComponent />
+              </AuthenticatedRoute>
+            }></Route>
           {/* http://localhost:8080/add-todo */}
           <Route
             path="/add-todo"
-            element={<TodoComponent />}></Route>
+            element={
+              <AuthenticatedRoute>
+                <TodoComponent />
+              </AuthenticatedRoute>
+            }></Route>
           {/* http://localhost:8080/update-todo/1 */}
           <Route
             path="/update-todo/:id"
-            element={<TodoComponent />}></Route>
+            element={
+              <AuthenticatedRoute>
+                <TodoComponent />
+              </AuthenticatedRoute>
+            }></Route>
+          {/* http://localhost:8080/register */}
+          <Route
+            path="/register"
+            element={<RegisterComponent />}></Route>
+
+          {/* http://localhost:8080/login */}
+          <Route
+            path="/login"
+            element={<LoginComponent />}></Route>
         </Routes>
         <FooterComponent />
       </BrowserRouter>
--- todo-ui/src/components/HeaderComponent.jsx
+++ todo-ui/src/components/HeaderComponent.jsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { isUserLoggedIn, logout } from "../services/AuthService";
+import { useNavigate } from "react-router-dom";
 
 const HeaderComponent = () => {
+  const isAuth = isUserLoggedIn();
+
+  const navigator = useNavigate();
+
+  function handleLogout() {
+    logout();
+    navigator("/login");
+  }
+
   return (
     <div>
       <header>
@@ -12,6 +24,51 @@ const HeaderComponent = () => {
               Todo Management Application
             </a>
           </div>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav">
+              {isAuth && (
+                <li className="nav-item">
+                  <NavLink
+                    to="/todos"
+                    className="nav-link">
+                    Todos
+                  </NavLink>
+                </li>
+              )}
+            </ul>
+          </div>
+          <ul className="navbar-nav">
+            {!isAuth && (
+              <li className="nav-item">
+                <NavLink
+                  to="/register"
+                  className="nav-link">
+                  Register
+                </NavLink>
+              </li>
+            )}
+
+            {!isAuth && (
+              <li className="nav-item">
+                <NavLink
+                  to="/login"
+                  className="nav-link">
+                  Login
+                </NavLink>
+              </li>
+            )}
+
+            {isAuth && (
+              <li className="nav-item">
+                <NavLink
+                  to="/login"
+                  className="nav-link"
+                  onClick={handleLogout}>
+                  Logout
+                </NavLink>
+              </li>
+            )}
+          </ul>
         </nav>
       </header>
     </div>
